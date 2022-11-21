257d23fa1f1974d80c8ead2723e288bb9ed48eeb
mikey017017 <118759234+mikey017017@users.noreply.github.com>
2022-11-22 01:17:19 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oArvm5oLN2S4pbIRBU9KNI0evme8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Am4JUpPlD0qR-xX6NvCNPZ_x0rFq388dxOhM_IlUQNM	',
+      useTemplateId: 'Am4JUpPlD0qR-xX6NvCNPZ_x0rFq388dxOhM_IlUQNM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
